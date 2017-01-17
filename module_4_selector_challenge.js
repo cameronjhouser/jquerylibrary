a8764f3ccde67cf5fa500179951e7e64ee5bc61c
Cameron Houser <Cameron Houser>
2017-01-17 13:39:11 -0500

    added a js challenge

---
+++ module_4_selector_challenge.js
@@ -0,0 +1,6 @@
+$(function(){
+$("#modifyThis").css({border: "5px solid red", color:"blue"});
+$("h3").css("background-color", "pink");
+$('#item-one li:first').css({border: "2px solid blue"});
+
+});
