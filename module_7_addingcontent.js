f72540544095174325a0824a3732af2591f3fc82
Cameron Houser <Cameron Houser>
2017-01-17 16:55:16 -0500

    started files on adding content injQ

---
+++ module_7_addingcontent.js
@@ -0,0 +1,16 @@
+$(function(){
+// content to be added
+var content = "<p style='color:red'>You can add me anywhere in the DOM using different jQuery methods!</p>'";
+var content2 = "You can add me anywhere in the DOM using different jQuery methods!";
+var content3 = "HA HA HA HA HA";
+
+// append
+$("#append-example").append(content);
+// prepend 
+$("#prepend-example").prepend(content);
+
+$("#before-example").before(content3);
+ 
+$("#after-example").after(content3);
+
+});
