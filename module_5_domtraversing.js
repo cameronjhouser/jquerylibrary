a41ed43cb1d9e2fb8c37b735098506c035813f87
Cameron Houser <Cameron Houser>
2017-01-17 14:19:21 -0500

    added dom js exercize

---
+++ module_5_domtraversing.js
@@ -0,0 +1,17 @@
+$(function(){
+	// next
+	$('#p-one').next().css({border: "2px solid blue"});
+	// prev
+	$('#list-two').prev().css({border: "2px solid red"});
+// parent
+	$("#p-two").parent().css({border: "2px solid yellow"});
+	// children
+	$("#div-two").children().css("font-style", "italic");
+// File
+$("#find").find("#facebook").css("background-color", "yellow");
+// another way to accomplish this would be 
+// $("#find #facebook").css("background-color", "yellow");
+
+// CLOSEt 
+$("#twitter-button").closest("li").css("background-color", "yellow");
+});
