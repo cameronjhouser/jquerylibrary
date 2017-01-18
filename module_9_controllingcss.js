c4c718fb8f63b8cb3b80a504976bbaec547f8b67
Cameron Houser <Cameron Houser>
2017-01-17 23:01:00 -0500

    jQ controlling css

---
+++ module_9_controllingcss.js
@@ -0,0 +1,13 @@
+$(function(){
+	// Checking value of CSS property 
+console.log($("#read-css").css("font-size"));
+// changing value of CSS property 
+$("#change-css-example").css("color", "blue");
+// changing multiple CSS property values using an object 
+$("#change-css-object-example").css({
+	"color": "green",
+	"background-color": "yellow",
+	"border": "2px solid red"
+});
+
+});
