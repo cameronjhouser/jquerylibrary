7ca9a6ba1a920c17a84354be8c6aaadb7a44a22c
Cameron Houser <Cameron Houser>
2017-01-21 11:13:15 -0500

    added files for a challenge

---
+++ module_11_manipulatedomchallenge.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+var content = "HA HA HA HA HA";
+
+$("#divText").append(content);
+
+$('#styled').css({
+    color: 'red',
+    fontSize: '39px'
+});
+
+$("#adding-class li").addClass('#adding-class');
+
+
+});
+
