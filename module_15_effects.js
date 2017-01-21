f8c9e044c432929a7c39b643949dfac7020c7091
Cameron Houser <Cameron Houser>
2017-01-21 13:02:52 -0500

    effects file

---
+++ module_15_effects.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+    $("#hide").click(function(){
+        $("#image").hide();
+    });
+
+    $("#show").click(function(){
+        $("#image").show();
+    });
+
+    $("#toggle").click(function(){
+        $("#image").toggle();
+    });
+
+    $("#fadeIn").click(function(){
+        $("#image").fadeIn(5000);
+    });
+
+    $("#fadeOut").click(function(){
+        $("#image").fadeOut(5000);
+    });
+
+    $("#fadeTo").click(function(){
+        $("#image").fadeTo(5000, .5);
+    });
+        $("#explode").click(function(){
+        $("#image").hide("explode", {pieces: 20}, 2000);
+    });
+
+    $("#updown").click(function(){
+        $("#image").slideUp(2000).slideDown(1000);
+    });
+
+    $("#sizechange").click(function(){
+        $("#image").animate({
+            height: '-=25%',
+            width:     '-=25%'
+        });
+    });
+    $("#sizeBigger").click(function(){
+        $("#image").animate({
+            height: '+=25%',
+            width:     '+=25%'
+        });
+    });
+});
